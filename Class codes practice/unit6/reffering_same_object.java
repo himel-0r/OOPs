513de069153261db0a3f9d5de24e677c6db2de33
himel-0r <himelchandraroy@gmail.com>
2023-02-25 01:54:21 +0600

    separated by unit

---
+++ Class codes practice/unit6/reffering_same_object.java
@@ -0,0 +1,30 @@
+package unit6;
+public class reffering_same_object {
+    public static void main(String[] args) {
+        Box b1 = new Box();
+
+        b1.h = 10;
+        b1.l = 20;
+        b1.w = 30;
+
+        Box b2 = b1;
+
+        b1.h = 11;
+        b1.l = 21;
+        b1.w = 31;
+
+        b1 = null;
+
+        System.out.println(b2.h + " " + b2.l + " " + b2.w);
+
+        // Once b2 gets the reference of b1, it does not matter if we make "b1 = null"
+        // The reference is unchanged.
+        
+    }
+}
+
+class Box {
+    int h; 
+    int w;
+    int l;
+}
---
+++ Class codes practice/unit6/static_method.java
@@ -0,0 +1,30 @@
+package unit6;
+import java.util.Scanner;
+
+public class static_method {
+
+    // A static method is a method that belongs to a 
+    // class rather than an instance of a class. This means
+    // you can call a static method without creating an object of 
+    // the class.
+    // Static methods are sometimes called class methods.
+
+
+    // You can access static methods from outside of the class in which
+    // they are defined. This is not possible with non-static methods.
+
+    // Subclasses can override static methods, but non-static methods cannot.
+
+    // Static methods are executed when an instance of the class is created, 
+    // whereas non-static methods are not.
+
+    // Static methods can be used to create utility classes that contain general-purpose methods.
+    
+    // You can use static methods to enforce encapsulation since they can only
+    // be called from within the class in which they are defined.
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        scan.close();
+    }
+}
