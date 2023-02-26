d5d1617cbe4b42026874bd9025f3bc5c80b1fd4d
himel-0r <himelchandraroy@gmail.com>
2023-02-27 03:16:29 +0600

    ColorBox added

--- Inheritance/Member_access/The_box.java
+++ Inheritance/Member_access/The_box.java
@@ -65,3 +65,15 @@ class BoxWeight extends Box {
         weight = m;
     }
 }
+
+// Here, Box is extended to include color.
+class ColorBox extends Box {
+    int color; // color of box
+
+    ColorBox(double w, double h, double d, int c) {
+        width = w;
+        height = h;
+        depth = d;
+        color = c;
+    }
+}
