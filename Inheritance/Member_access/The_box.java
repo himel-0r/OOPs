cdc875619fda3de173d5e297a2b564baf1aeaff1
himel-0r <himelchandraroy@gmail.com>
2023-02-27 03:20:08 +0600

    RefDemo class added

--- Inheritance/Member_access/The_box.java
+++ Inheritance/Member_access/The_box.java
@@ -77,3 +77,32 @@ class ColorBox extends Box {
         color = c;
     }
 }
+
+// This class is added for nothing.
+
+class RefDemo {
+    public static void mamin(String[] args) { // named as mamin so that no error :D
+        BoxWeight weightbox = new BoxWeight(3, 5, 7, 8.37);
+        Box plainbox = new Box();
+        double vol;
+        vol = weightbox.volume();
+
+        System.out.println("Volume of weightbox is " + vol);
+        System.out.println("Weight of weightbox is " +
+                weightbox.weight);
+        System.out.println();
+
+        // assign BoxWeight reference to Box reference
+        plainbox = weightbox;
+
+        vol = plainbox.volume(); // OK, volume() defined in Box
+        System.out.println("Volume of plainbox is " + vol);
+
+        /*
+         * The following statement is invalid because plainbox
+         * does not define a weight member.
+         */
+        
+        // System.out.println("Weight of plainbox is " + plainbox.weight);
+    }
+}
