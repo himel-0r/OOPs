1007f09b4036d03307791b19833fed399bad39b4
himel-0r <himelchandraroy@gmail.com>
2023-03-07 01:30:58 +0600

    sdhcbljkxhbjkbISh

--- Inheritance/Abstract_class/AbstractDemo.java
+++ Inheritance/Abstract_class/AbstractDemo.java
@@ -1,30 +1,66 @@
 package OOPs.Inheritance.Abstract_class;
+// A java program to demonstrate use of
 
+// static keyword with methods and variables
+
+// Driver class
 public class AbstractDemo {
     public static void main(String[] args) {
-        B b = new B();
-        b.callme();
-        b.callmetoo();
-        // A aoj = b;
-        // aoj.callmetoo();
+        // calling static method
+        // without instantiating Student class
+        Student.setCllg("XYZ");
 
-        Object abc = b;
-        System.out.println(abc.getClass());
+        Student s1 = new Student("Alice");
+        Student s2 = new Student("Bob");
+
+        s1.getStudentInfo();
+        s2.getStudentInfo();
+
+        System.out.println(Student.setRollNo());
     }
 }
 
-// A Simple demonstration of abstract.
-abstract class A {
-    abstract void callme();
+// Student class
+class Student {
+    String name;
+    int rollNo;
 
-    // concrete methods are still allowed in abstract classes
-    void callmetoo() {
-        System.out.println("This is a concrete method.");
+    // static variable
+    static String cllgName;
+
+    // static counter to set unique roll no
+    static int counter = 0;
+
+    public Student(String name) {
+        this.name = name;
+
+        this.rollNo = setRollNo();
+    }
+
+    // getting unique rollNo
+    // through static variable(counter)
+    static int setRollNo() {
+        counter++;
+        return counter;
+    }
+
+    // static method
+    static void setCllg(String name) {
+        cllgName = name;
+    }
+
+    // instance method
+    void getStudentInfo() {
+        System.out.println("name : " + this.name);
+        System.out.println("rollNo : " + this.rollNo);
+
+        // accessing static variable
+        System.out.println("cllgName : " + cllgName);
     }
 }
 
-class B extends A {
-    void callme() {
-        System.out.println("B's implementation of callme.");
-    }
-}
+// ajke onek jinis shikhtesi
+// vogoban, eisob jodi kalke mone na thake tahole ami shesh.
+// ajker porikkha voyaboho hoise, kalke jeno valo hoy
+// indeed, next gula jeno valo hoy.
+// please.
