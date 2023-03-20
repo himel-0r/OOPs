f95e84708abd763af1bcf81fb5ce3dce49e5d0be
himel-0r <himelchandraroy@gmail.com>
2023-03-21 02:52:26 +0600

    Added code :v

---
+++ Exception_Handling/practice1.java
@@ -0,0 +1,11 @@
+package OOPs.Exception_Handling;
+
+public class practice1 {
+    public static void main(String[] args) {
+        int d = 0;
+
+        int a = 42/d;
+
+        System.out.println(a);
+    }
+}
--- Package/package_practice.java
+++ Package/package_practice.java
@@ -1,5 +1,5 @@
 class package_practice{
     public static void main(String[] args) {
-        
+
     }
 }
