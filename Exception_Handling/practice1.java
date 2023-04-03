ad886ea4f95f58dd231e6624e257c9f07a5ffc68
himel-0r <himelchandraroy@gmail.com>
2023-04-04 01:11:02 +0600

    Collection Framework

--- Exception_Handling/practice1.java
+++ Exception_Handling/practice1.java
@@ -4,8 +4,16 @@ public class practice1 {
     public static void main(String[] args) {
         int d = 0;
 
+        try{
         int a = 42/d;
 
         System.out.println(a);
+        }
+        catch (Exception e) {
+            System.out.println("Exception is " + e);
+        }
+        finally {
+            System.out.println("This is finally.");
+        }
     }
 }
