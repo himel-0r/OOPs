e61183bccf2110ebf216d1e2d9036214be5622e4
himel-0r <himelchandraroy@gmail.com>
2023-04-04 12:47:52 +0600

    Added teacher's codes

---
+++ Collection_Frameworks/collections.java
@@ -0,0 +1,19 @@
+package OOPs.Collection_Frameworks;
+
+import java.util.Collection;
+import java.util.Vector;
+
+public class collections {
+    Vector<Integer> v = new Vector<Integer>();
+
+    for (int i=1; i<=5; i++) {
+        v.add(i);
+    }
+
+    v.add(10);
+
+    
+
+    
+
+}
